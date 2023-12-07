5fac5b2a8181d55468d6dfb944e97bfe8d0efc4b
lkodovski <l.kodovski@hotmail.com>
2023-12-07 11:32:25 +0100

    Objects

--- script.js
+++ script.js
@@ -167,3 +167,105 @@ checkWinner(scoreDolphins, scoreKoalas);
 // const age4 = calcAge(years[3]);
 // const ages1 = [age1, age2, age3, age4];
 // console.log(ages1);
+
+//Basic Array Methods
+
+// const friends = ["Michael", "Steven", "Peter"];
+
+// const newLength = friends.push("Jay"); // dodava element na nizata
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift("John"); //dodava element na pocetok na niza
+// console.log(friends);
+
+// friends.pop(); //brise posleden
+// console.log(friends);
+
+// friends.shift(); //brise prv
+// console.log(friends);
+
+// console.log(friends.indexOf("Steven")); // vrakja index na elementot vo nizata
+// console.log(friends.indexOf("Bob"));
+
+// if (friends.includes("Steven")) {
+//   console.log("You have a friend called Steven");
+// }
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+// console.log(bills, tips, total);
+
+// Introduction to Objects
+
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Smethman",
+//   job: "teacher",
+//   friends: ["Michael", "Steven", "Peter"],
+// };
+
+// console.log(jonas);
+
+// console.log(jonas.lastName, jonas.firstName);
+
+// // Dodavanje svojstva
+// jonas.location = "Portugal";
+// console.log(jonas);
+
+// Objects method
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Smethman",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Steven", "Peter"],
+  hasDriverLicense: true,
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // },
+  // calcAge: function () {
+  //   console.log(this);
+  //   return 2037 - this.birthYear;
+  // },
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()} years old`;
+  },
+};
+
+console.log(jonas.calcAge());
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+console.log(jonas.getSummary());
